409edfe040e542fb8d7530c077cfec343beece4f
xfaon <xfaonae@protonmail.com>
2022-07-12 01:49:29 -0400

    The big bang!! ❤️

---
+++ cmake-build-debug/deps/bkaradzic/bgfx.cmake/generated/shader.cpp
@@ -0,0 +1,4 @@
+#include "/home/xfaon/CLionProjects/BgfxHelloWorld/deps/bkaradzic/bgfx.cmake/bgfx/src/shader.cpp"
+#include "/home/xfaon/CLionProjects/BgfxHelloWorld/deps/bkaradzic/bgfx.cmake/bgfx/src/shader_dx9bc.cpp"
+#include "/home/xfaon/CLionProjects/BgfxHelloWorld/deps/bkaradzic/bgfx.cmake/bgfx/src/shader_dxbc.cpp"
+#include "/home/xfaon/CLionProjects/BgfxHelloWorld/deps/bkaradzic/bgfx.cmake/bgfx/src/shader_spirv.cpp"
---
+++ cmake-build-debug/deps/bkaradzic/bgfx.cmake/generated/vertexlayout.cpp
@@ -0,0 +1,1 @@
+#include "/home/xfaon/CLionProjects/BgfxHelloWorld/deps/bkaradzic/bgfx.cmake/bgfx/src/vertexlayout.cpp"
